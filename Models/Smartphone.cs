dab30df38085f8116dcc5a0970b96371f478f176
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2024-05-30 10:03:28 -0300

    UPDATE classes e Main

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,15 +2,46 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        
+         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+        
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _Modelo;
+        private string _IMEI;
+        private int _Memoria;
+
+        public string Modelo
+        {
+            get => _Modelo;
+
+            set{
+                _Modelo = value;
+            }
         }
 
+        public string IMEI{
+            get => _IMEI;
+
+            set{
+                _IMEI = value;
+            }
+        }
+
+        public int Memoria{
+            get => _Memoria;
+
+            set{
+                _Memoria = value;
+            }
+        }
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "789456", modelo: "X", imei: "2222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
